59267619390a0a86d68ec0ee8d0419458de45a10
lexa <lexa@gmail.com>
2024-01-26 11:49:49 +0330

    Number guessing game

--- project6.py
+++ project6.py
@@ -1,18 +1,18 @@
-#Number guessing game
+# Number guessing game
 import random
 
 answer = random.randint(1,100)
-print(answer)
+print(">>>",answer,"<<<")
 print("...........................................\n")
 number = int(input("Please enter a number in range (1-100) :"))
 
 while number != answer:
     if number > answer:
-        print(">>> It's smaller")
+        print(">>> It's Smaller...")
         print("...........................................\n")
 
     else:
-        print(">>> It's bigger")
+        print(">>> It's Bigger...")
         print("...........................................\n")
 
 
