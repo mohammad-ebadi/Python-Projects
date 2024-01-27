9be7c8f3b6b5180db82b50f7267190a3a5fc9559
lexa <lexa@gmail.com>
2024-01-27 11:27:52 +0330

    Hello Programm

---
+++ project8.py
@@ -0,0 +1,11 @@
+# Hello Programm
+name_1 = input(">>> Hello , enter you're name please :")
+
+if name_1 == "Mohammad":
+    print(">>> Hi , Mohammad <<<")
+elif name_1 == "Peter":
+    print(">>> Hi , Peter <<<")
+else:
+    name_2 = input("Who are you ?, enter you're name please again :")
+
+    print("Helooo",name_2)
