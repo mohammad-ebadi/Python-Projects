099c7966c969688d33267d957f8ed0e60c5341f7
Mohammad Ebadi <mohammad.ebadi.code@gmail.com>
2024-01-27 10:41:38 +0330

    Update project5.py

--- project5.py
+++ project5.py
@@ -1,4 +1,4 @@
-#Cut text from character number zero(0) until character DOT(.)
+#Cut a text from character number zero(0) until character DOT(.)
 text=input("Enter text that contains character DOT(.) inside :")
 s=text.find(".")
 print(text[0:s+1])
