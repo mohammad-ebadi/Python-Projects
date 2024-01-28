d334c934b90fc16c039ff61a97b61ad3f87012e6
lexa <lexa@gmail.com>
2024-01-28 11:50:57 +0330

    deleted 3 files

--- project10.py
+++
@@ -1,5 +0,0 @@
-# welcome Program v.2
-name = input("Enter you're name please :")
-while name != "end":
-    print("Welcome ",name)
-    name = input("Enter you're name please :")
--- project11.py
+++
@@ -1,6 +0,0 @@
-# Limitless loop by using (While)
-n=1
-while n != 0:
-    print(n)
-    n=n+1
-    
