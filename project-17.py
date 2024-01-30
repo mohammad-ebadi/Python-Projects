19ca079c9f953b5d7f4bdd19c909e884b62de24d
Mohammad Ebadi <mohammad.ebadi.code@gmail.com>
2024-01-30 18:46:40 +0330

    Working with Functions v.2

---
+++ project-17.py
@@ -0,0 +1,12 @@
+# Working with Functions v.2
+def user_info(name,last_name,country,number):
+    print("Name :",name, "\nLast-Name :",last_name,"\nContry :",country,"\nPhone-Number :",number )
+    
+    
+#input(user_info("Mohammad","Ebadi","USA","+19931992"))
+name=input("Name : ")
+last_name=input("\nLast-Name : ")
+country=input("\nCountry : ")
+number=input("\nPhone-Number : ")
+user_info(name,last_name,country,number)
+
