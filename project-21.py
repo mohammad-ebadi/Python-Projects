aed5e31765417adf49993d1cad4586fd249716cf
Mohammad Ebadi <mohammad.ebadi.code@gmail.com>
2024-01-31 13:11:39 +0330

    Collecting Names Using lists

--- project-21.py
+++ project-21.py
@@ -1,11 +1,13 @@
 # Collecting Names Using lists
 name = "pass"
 name_list = []
+count=0
 while name != "":
     name = input(">>> Enter you're Name &  >>> For save press (ENTER) bottom : ")
+    count=count+1
     if name == "":
         break
     else:
         name_list.append(name)
         
-print(">>> Members Name : ",name_list)
+print(">>> Members Name and Count : ","(",count-1, ")",name_list)
