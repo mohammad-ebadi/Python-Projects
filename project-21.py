1a8d4212be3fc702b361e548916d869a52f3ca79
Mohammad Ebadi <mohammad.ebadi.code@gmail.com>
2024-01-31 17:02:51 +0330

    Collecting Names Using lists

--- project-21.py
+++ project-21.py
@@ -3,11 +3,14 @@ name = "pass"
 name_list = []
 count = 0
 while name != "":
+    
     name = input(">>> Enter you're Name OR  >>> For Continue press (ENTER) bottom : ")
     count = count + 1
     if name == "":
+        
         break
     else:
+        
         name_list.append(name)
     
 print(">>> Changs saved  <<< Members Name(s) and Count : ","(",count-1, ")",name_list)
@@ -16,28 +19,32 @@ print(">>> Changs saved  <<< Members Name(s) and Count : ","(",count-1, ")",name
 
 idea = ""
 while idea != 3:
+    
     idea = int(input("### ADD a NEW NAME press(1):\n### REMOVE a NAME press(2):\n### SAVE and EXITE press(3):"))
 
     if idea == 2:
         
         remove = input("\nEnter you're name for remove :")
+        
         if remove not in name_list:
+            
             print(">>> ",remove," Not Found !!!")
         else:
+            
             index = name_list.index(remove)
             del name_list[index]
             print(remove," # Deleted #")
-        
         
     elif idea == 1:
         
         name = input(">>> Enter you're Name OR  >>> For Continue press (ENTER) bottom : ")
         
         if name == "":
+            
             break
         
         else:
+            
             name_list.append(name)
-        
 
 print(">>> Changs saved  <<< Members Name(s) and Count : ","(",count-1, ")",name_list)
