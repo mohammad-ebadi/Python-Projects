fd99673e9b69e433b7e91310edcacd88d5cddbb7
Mohammad Ebadi <mohammad.ebadi.code@gmail.com>
2024-02-07 05:10:33 -0800

    Determining whether numbers are even or not using functions

---
+++ project-28.py
@@ -0,0 +1,9 @@
+# Determining whether numbers are even or not using functions
+def x(num):
+    if num % 2 == 0 :
+        print("Even")
+    else:
+        print("Odd")
+
+num=int(input("Enter a number : "))
+x(num)
